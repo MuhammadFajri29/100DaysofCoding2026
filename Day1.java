6cad5e8d858de60856cfe5a682f127304a5824c8
MuhammadFajri29 <epangze@gmail.com>
2026-09-02 21:39:38 +0800

    Create Day1.java

---
+++ Day1.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        // Komentar satu baris
+        /* Komentar beberapa baris */
+        /** Komentar dokumentasi */
+
+        System.out.println("Halo, Dunia");
+        System.out.println("Day 1 Mempelajari Struktur Dasar Java & menggunakan semua komentar");
+    }
+}

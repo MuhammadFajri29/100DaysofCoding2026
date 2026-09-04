6cf2d69bb09a87b2c2cd551c37423aecba9aff0b
MuhammadFajri29 <epangze@gmail.com>
2026-09-04 16:27:22 +0800

    Create Day3.java

---
+++ Day3.java
@@ -0,0 +1,21 @@
+public class Days3 {
+    public static void main(String[] args) {
+
+        // Escape sequence adalah karakter khusus dalam Java yang diawali dengan tanda backslash (\). Escape sequence digunakan untuk memberikan fungsi tertentu di dalam String, misalnya membuat baris baru, tab, atau menampilkan tanda kutip.
+        // \n: Digunakan untuk pindah ke baris baru, seperti menekan Enter
+        // \t: Digunakan untuk memberikan jarak/tab
+        // \": Digunakan untuk menampilkan tanda kutip ganda di dalam string
+        // \\: Digunakan untuk menampilkan karakter backslash
+
+        System.out.println("Nama: Muhammad Fajri \nNIM: H0725004");
+
+        System.out.println("Hobi\t: Main Game Mobile Legends");
+        System.out.println("Alamat\t: Lingk. Pakkola");
+        System.out.println("Jurusan\t: Pendidikan Teknologi Informasi\n");
+
+        System.out.println("Semuanya berkata \"Hala Madrid\"");
+
+        System.out.println("Alamat File:C:\\Users\\Muh.Fajri\\Dokumen Days3");
+        
+    }
+}

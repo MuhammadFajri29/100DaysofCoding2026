bc44b0c43478d8249f25f2130762bcbe7638d3a2
MuhammadFajri29 <epangze@gmail.com>
2026-09-07 16:41:17 +0800

    Create day6.java

---
+++ day6.java
@@ -0,0 +1,24 @@
+public class Days6 {
+    public static void main(String[] args) {
+        
+        String Nama = "Muhammad Fajri";
+        char Goldar = 'O';
+        boolean Status = true;
+
+        // char adalah tipe data primitif yang nilai nya di apit tanda kutip 1
+        // char adalah tipe data yang hanya menyimpan satu karakter
+
+        //boolean adalah tipe data primitif yang hanya bernilai benar dan salah
+        // true and false
+        //boolean juga tidak perlu di apit oleh tanda kutip
+
+        System.out.println("===============================");
+        System.out.println("DATA MAHASISWA UNSULBAR");
+        System.out.println("===============================");
+        System.out.println("Nama\t\t:" + Nama);
+        System.out.println("Golongan Darah\t:" + Goldar);
+        System.out.println("Status Mahasiswa Aktif:" + Status);
+        System.out.println();
+        System.out.println("==========TERIMAKASIH==========");
+    }
+}

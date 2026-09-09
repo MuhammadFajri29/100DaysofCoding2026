e86578a792567f4729ebbf24d31eef185d5eb328
MuhammadFajri29 <epangze@gmail.com>
2026-09-09 18:27:13 +0800

    Create Day8.java

---
+++ Day8.java
@@ -0,0 +1,20 @@
+public class Days8 {
+    public static void main(String[] args) {
+
+        // Deklarasi adalah proses membuat atau menentukan sebuah variabel dengan menyebutkan tipe datanya dan nama variabelnya, tetapi belum memberikan nilai.
+        int umur;
+
+        // Inisialisasi adalah memberikan nilai awal kepada variabel.
+        umur = 20;
+        
+
+        System.out.println("Umur awal\t: " + umur);
+        
+
+        // Update adalah mengubah nilai variabel yang sudah ada.
+        umur = 21;
+
+        System.out.println("Umur update\t: " + umur);
+        
+    }
+}

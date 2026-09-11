a89e292fafe4062280591f860d4dc38bf8fd19a9
MuhammadFajri29 <epangze@gmail.com>
2026-09-11 08:38:13 +0800

    Create Day10.java

---
+++ Day10.java
@@ -0,0 +1,22 @@
+public class Days10 {
+    public static void main(String[] args) {
+
+        //Setiap tipe data numerik di Java memiliki batas minimal (minimum) dan batas maksimal (maximum).
+
+        // Batas minimum dan maksimum tipe data byte 8 bit
+        System.out.println("Byte minimum: " + Byte.MIN_VALUE);
+        System.out.println("Byte maksimum: " + Byte.MAX_VALUE);
+
+        // Batas minimum dan maksimum tipe data short 16 bit
+        System.out.println("Short minimum:" + Short.MIN_VALUE);
+        System.out.println("Short maksimum:" + Short.MAX_VALUE);
+
+        // Batas minimum dan maksimum tipe data int 32 bit
+        System.out.println("Int minimum: " + Integer.MIN_VALUE);
+        System.out.println("Int maksimum: " + Integer.MAX_VALUE);
+
+        // Batas minimum dan maksimum tipe data double 64 bit
+        System.out.println("Double minimum: " + Double.MIN_VALUE);
+        System.out.println("Double maksimum: " + Double.MAX_VALUE);
+    }
+}

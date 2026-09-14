873ea7afb45fe60dbc4ffc2c4384e3cf4ca031ed
MuhammadFajri29 <epangze@gmail.com>
2026-09-14 21:30:52 +0800

    Evaluasi M1 Day013.java

---
+++ Day013.java
@@ -0,0 +1,14 @@
+public class evaluasi4 {
+    public static void main(String[] args) {
+        final String NAMA_TOKO = "Toko Sejahtera";
+        int h = 50000;
+
+        System.out.println("Toko: " + NAMA_TOKO);
+        System.out.println("Harga Awal: " + h);
+
+        h = 65000;
+
+        System.out.println("Harga Setelah Update: " + h);
+
+    }
+}

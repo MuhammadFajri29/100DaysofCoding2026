b1c3c8db7654732b6fdf80cfa9c808b8b649236c
MuhammadFajri29 <epangze@gmail.com>
2026-09-15 22:06:50 +0800

    Days14.java

---
+++ Days14.java
@@ -0,0 +1,13 @@
+public class Days14 {
+
+    public static void main(String[] args) {
+        int a = 29;
+        int b = 16;
+
+        int penjumlahan = a + b;
+        int pengurangan = a - b;
+
+        System.out.println("Hasil Penjumlahan = " + penjumlahan);
+        System.out.println("Hasil Pengurangan = " + pengurangan);
+    }
+}

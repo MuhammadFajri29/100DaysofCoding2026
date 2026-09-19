9cf20caeeb0ac34d9b3ab49688bfe9551844de77
MuhammadFajri29 <epangze@gmail.com>
2026-09-19 20:19:42 +0800

    Day018.java

---
+++ Day018.java
@@ -0,0 +1,22 @@
+public class Days18 {
+    public static void main(String[] args) {
+
+        byte a = 29;
+        short b = a;
+        int c = b;
+        long d = c;
+        float e = d;
+        double f = e;
+
+        System.out.println("=== Konversi Otomatis ===");
+        System.out.println("byte  : " + a);
+        System.out.println("short : " + b);
+        System.out.println("int   : " + c);
+        System.out.println("long  : " + d);
+        System.out.println("float : " + e);
+        System.out.println("double: " + f);
+        
+        
+
+    }
+}
